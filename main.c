ec99a339616c66cdbe9373e3cfe0654263c10a76
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2022-03-24 15:50:06 +0800

    teams struct

--- main.c
+++ main.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+struct Project{
+    char *name;     // name of the project
+    char *manager;  // only 1 manage
+    char *member[3];    // three member
+};
+struct Team{
+    char *name;                 // the name of the teams
+    struct Project projects[];  // the list of project in the teams
+};
+
+struct Team *teams[];
+
 int main(int argc, char *argv[]){
     printf("hi");
 }
+
+// void create_project(char *project_name){
+//     teams.projects.name = project_name;
+// }
