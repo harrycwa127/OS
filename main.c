7b64b417722d8193c867668eec60a18d71c3b18e
harrycwa127 <66370343+harrycwa127@users.noreply.github.com>
2022-03-24 15:53:00 +0800

    midify struct

--- main.c
+++ main.c
@@ -2,14 +2,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-struct Project{
-    char *name;     // name of the project
+struct Team{
+    char *team_name;                 // the name of the teams
+    // struct Project projects[];  // the list of project in the teams
+    char *project_name;     // name of the project
     char *manager;  // only 1 manage
     char *member[3];    // three member
-};
-struct Team{
-    char *name;                 // the name of the teams
-    struct Project projects[];  // the list of project in the teams
 };
 
 struct Team *teams[];
