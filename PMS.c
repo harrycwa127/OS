bcad788ef227b399959701cc22a28905108358a2
tung9898 <hoyiutung@hotmail.com>
2022-04-02 16:51:23 +0800

    Menu Part 1 Big Update

--- PMS.c
+++ PMS.c
@@ -2,10 +2,7 @@
 #include <string.h>
 #include "team.c"
 
-int main(){
-    char choice[2], team_name[50], project_name[50], manager[50], member[3][50];
-    int boolean = 1; //0 = flase, 1 = true
-
+void PrintMenu(){
     printf("\n\t~~ WELCOME TO PolyStar ~~\n\n");
     printf("1.\tCreate Project Team\n\n");
     printf("2.\tProject Meeting Request\n");
@@ -17,53 +14,48 @@ int main(){
     printf("\t3b.  XXXX (Another algorithm implemented)\n");
     printf("\t3c.  YYYY (Attendance Report)\n\n");
     printf("4.\tExit\n");
-    
-    do{
-    printf("  Enter an option: ");
-    scanf("%s", choice);
+}
 
-    if (strcmp(choice, "1") == 0){
-        printf("choice %s\nEnter> ", choice);
-        boolean = 1;
+int main(){
+    Team_Init();
+    while(1){
+        char choice[2];
+        PrintMenu();
+        printf("  Enter an option: ");
+        scanf("%s", choice);
+        if (strcmp(choice, "0") == 0){
 
-        scanf("%s %s %s %s %s %s", team_name, project_name, manager, member[0], member[1], member[2]);
-        printf("testing: %s %s %s\n", &member[0], &member[1], &member[2]);
-        create_team(team_name, project_name, manager, member);
-    }
-    else if (strcmp(choice, "2a") == 0){
-        printf("choice %s\n", choice);
-        boolean = 1;
-    }
-    else if (strcmp(choice, "2b") == 0){
-        printf("choice %s\n", choice);
-        boolean = 1;
-    }
-    else if (strcmp(choice, "2c") == 0){
-        printf("choice %s\n", choice);
-        boolean = 1;
-    }
-    else if (strcmp(choice, "3a") == 0){
-        printf("choice %s\n", choice);
-        boolean = 1;
-    }
-    else if (strcmp(choice, "3b") == 0){
-        printf("choice %s\n", choice);
-        boolean = 1;
-    }
-    else if (strcmp(choice, "3c") == 0){
-        printf("choice %s\n", choice);
-        boolean = 1;
-    }
-    else if (strcmp(choice, "4") == 0)
-    {
-        printf("choice %s\n", choice);
-        return 0;
-    }
-    else{
-        printf("Unaccepted input, please input the correct input.\n");
-        printf("If you chooces the option 2a, please input \"2a\" instead of \"2\" only\n");
-        boolean = 0;
-    }
-    }while(boolean == 0);
-    return 0;
+        }else if (strcmp(choice, "1") == 0){
+            printf("choice %s\n", choice);      //debug use
+            while(1){
+                char team_name[50], project_name[50], manager[50], member[3][50];
+                printf("Enter> ");
+                scanf("%s", team_name);
+                if (strcmp(team_name, "0") == 0) break;
+                scanf("%s", project_name);
+                scanf("%s", manager);
+                scanf("%s", member[0]);
+                scanf("%s", member[1]);
+                scanf("%s", member[2]);
+                // printf("%s %s %s %s %s %s\n", team_name, project_name, manager, member[0], member[1], member[2]);
+                create_team(team_name, project_name, manager, member);
+            }
+        }else if (strcmp(choice, "2a") == 0){
+            printf("choice %s\n", choice);      //debug use
+        }else if (strcmp(choice, "2b") == 0){
+            printf("choice %s\n", choice);      //debug use
+        }else if (strcmp(choice, "2c") == 0){
+            printf("choice %s\n", choice);      //debug use
+        } else if (strcmp(choice, "3a") == 0){
+            printf("choice %s\n", choice);      //debug use
+        } else if (strcmp(choice, "3b") == 0){
+            printf("choice %s\n", choice);      //debug use
+        } else if (strcmp(choice, "3c") == 0){
+            printf("choice %s\n", choice);      //debug use
+        } else if (strcmp(choice, "4") == 0){
+            printf("choice %s\n", choice);      //debug use
+            break;
+        } else{ printf("Unaccepted input, please input the correct input.\n"); }
+    }   
+    return(0);
 }
--- team.c
+++ team.c
@@ -3,15 +3,61 @@
 #include <stdbool.h>
 #include <time.h>
 
+#define team_size 1000
+
 struct Team{
-    char *team_name;                 // the name of the teams
-    // struct Project projects[];   // the list of project in the teams
-    char *project_name;             // name of the project
-    char *manager;                  // only 1 manage
-    char member[3][50];                // three member
+    int team_id;
+    char team_name[50];
+    char project_name[50];
+    char manager[50];
+    char member[3][50];
     time_t dateTime;
     float duration;
-};
+} Team;
+
+struct Team teams[team_size];
+
+void Team_Init(){
+    int i;
+    for(i=0;i<team_size;i++){
+        teams[i].team_id = -1;
+        strcpy(teams[i].team_name, "");
+        strcpy(teams[i].project_name, "");
+        strcpy(teams[i].manager, "");
+        strcpy(teams[i].member[0], "");
+        strcpy(teams[i].member[1], "");
+        strcpy(teams[i].member[2], "");
+        teams[i].dateTime = 0;
+        teams[i].duration = -1;
+    }
+}
+
+void print_team_name(int i){
+    printf("team id %d\n", teams[i].team_id);
+    printf("team_name %s\n", teams[i].team_name);
+    printf("project_name %s\n", teams[i].project_name);
+    printf("manager %s\n", teams[i].manager);
+    printf("member %s\n", teams[i].member[0]);
+    printf("member %s\n", teams[i].member[1]);
+    printf("member %s\n", teams[i].member[2]);
+}
+
+void create_team(char team_name[50], char project_name[50], char manager[50], char member[3][50]){
+    int i;
+    for(i=0;i<sizeof(teams);i++)
+        if(teams[i].team_id == -1){
+            teams[i].team_id = i;
+            strcpy(teams[i].team_name, team_name);
+            strcpy(teams[i].project_name, project_name);
+            strcpy(teams[i].manager, manager);
+            strcpy(teams[i].member[0], member[0]);
+            strcpy(teams[i].member[1], member[1]);
+            strcpy(teams[i].member[2], member[2]);
+            // print_team_name(i);
+            printf("Project Team %s is created.\n", team_name);
+            break;
+        }
+}
 
 time_t dateTimeFormatter(char date[10], char time[5])
 {
@@ -28,27 +74,4 @@ time_t dateTimeFormatter(char date[10], char time[5])
             printf("Could not convert time input to time_t\n");
     } else printf("The input was not a valid time format\n");
     return dateTime;
-}
-
-struct Team *teams[1000];
-
-void print_team_name(int i){
-    printf(">>>>>> Project Team %s is created\n", teams[i]->team_name);
-
-    //testing
-    printf("Detail: %s %s %s %s %s\n", teams[i]->project_name, teams[i]->manager, teams[i]->member[0], teams[i]->member[1], teams[i]->member[2]);
-}
-
-void create_team(char *team_name, char *project_name, char *manager, char member[3][50]){
-    struct Team temp = { .team_name=team_name, .project_name=project_name, .manager=manager, .member=**member };  // decalre new Team for append
-
-    // findout the last element for append
-    int i = 0;
-    while(teams[i] != NULL){
-        i++;
-    }
-
-    //append
-    teams[i] = &temp;
-    print_team_name(i);
 }
