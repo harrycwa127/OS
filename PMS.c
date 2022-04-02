af3ca6abd2b12795b3890162050794fa75d2e91b
jimleungcc <jimleungcc@gmail.com>
2022-04-03 03:06:00 +0800

    feat: finish .dat file input without repeat checking

--- PMS.c
+++ PMS.c
@@ -95,19 +95,37 @@ int main(){
             // check file
             if (file == NULL) exit(EXIT_FAILURE);
 
-            // read file line by line
-            // while ((read = getline(&line, &len, file)) != -1) {
-            //     printf("Retrieved line of length %zu:\n", read);
-            //     char buffer[50];
-            //     fgets(buffer, 50,file);
-            //     printf("Content: %s\n", buffer);
-            // }
-            char buffer[50];
+            char buffer[500];
             int lineNum = 0;
-            while(fgets(buffer, 50, file)!=NULL)
+            while(fgets(buffer, 500, file)!=NULL)
             {                
                 lineNum+=1;
                 printf("Content: %s in line %d\n", buffer,lineNum);
+
+                // if(buffer[0] == '0') break;
+                char *ptr = strtok(buffer, " ");
+                char storage[4][50];
+                int i;
+                
+                for(i=0;i<4;i++){ strcpy(storage[i], "");}
+                for(i=0;i<4;i++){
+                    strcpy(storage[i], ptr);
+                    ptr = strtok(NULL, " ");
+                    if(ptr == NULL) break;
+                }
+
+                // Start booking
+                if(strlen(storage[1]) == 10 && strlen(storage[2]) == 5){
+                    char date[11];
+                    char time[6];
+                    strcpy(date, storage[1]);
+                    strcpy(time, storage[2]);
+                    date[10] = '\0';
+                    time[5] = '\0';
+                    project_booking(storage[0], date, time,atoi(storage[3]));
+                }else{
+                    printf("Unaccepted input, please input the correct input.\n");
+                }
             }
             
 
@@ -115,6 +133,16 @@ int main(){
             //close file after read done
             fclose(file);
             if (line) free(line);
+
+            char input[70];
+            printf("Enter 0 to back to main menu ");
+            while (1)
+            {
+                fgets(input,70,stdin);
+                if(input[0] == '0') break;
+            }
+            
+            
                 
         }else if (strcmp(choice, "2c") == 0){
             printf("choice %s\n", choice);      //debug use
